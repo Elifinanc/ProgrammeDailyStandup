53f5d04521b11262b0902a91d11282af4f149530
Elifinanc <58734605+Elifinanc@users.noreply.github.com>
2020-01-10 09:20:35 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OrdreDailyStandup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nomsPersonnes = new string[9] { "Corentin", "Anatolie", "Elisa", "Laure", "Kevin", "Elif", "Iurii", "Jean", "Laurent" }; 
+
+            string personneStockee;
+
+            Random aleatoire = new Random();
+            int personneAleatoire;
+
+            int nbPersonne = nomsPersonnes.Length;
+            
+            for (int i = 0; i < nbPersonne; i++) {
+                personneStockee = nomsPersonnes[i]; // On stocke la peronne d'indice i 
+                personneAleatoire = aleatoire.Next(i, nbPersonne); // Génère un entier compris entre i et le nombre total d'élement du tableau 
+                nomsPersonnes[i]= nomsPersonnes [personneAleatoire]; // On affecte la place de la personne d'indice i à la personne dont l'indice a été pioché par notre programme de nb aléatoire
+                nomsPersonnes[personneAleatoire]= personneStockee; // On affecte la place de cette persionne piochée à la personne stocké au début
+
+                Console.WriteLine(nomsPersonnes [i]);
+            }
+       
+        }
+    }
+}
